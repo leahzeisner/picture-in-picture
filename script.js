3bb50fee95f01a440a7eb1716e212b87e7060341
leahzeisner <zeisner.l@northeastern.edu>
2021-06-11 23:01:28 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// DOM Elements
+
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+
+
+
+// Prompt to select media stream, pass to video element, play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+        videoElement.srcObject = mediaStream
+        videoElement.onloadedmetadata = () => {
+            videoElement.play()
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+
+button.addEventListener('click', async () => {
+    button.disabled = true
+
+    // Start Picture In Picture
+    await videoElement.requestPictureInPicture()
+
+    button.disabled = false
+})
+
+
+
+
+// On Load
+selectMediaStream()
